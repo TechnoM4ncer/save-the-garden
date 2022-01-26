ce027288e21fe67e6d12a1fd9300dbdc560d8817
TechnoM4ncer <kinodekirakashi@live.com>
2022-01-26 16:21:03 -0600

    Add content to navbar

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,10 +1,24 @@
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
-import React from "react";
+import { AppBar, Toolbar, Typography, IconButton, Badge } from "@material-ui/core";
+import { Menu as MenuIcon } from "@material-ui/icons";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import './Header.css';
 
+const itemCount = 0;
 export default function Header() {
     const displayDesktop = () => {
-    return <Toolbar></Toolbar>;
+    return ( 
+    <AppBar position="static" className={"header"}>
+    <Toolbar>
+      <IconButton edge="start" color="inherit" aria-label="menu" className={"menu"}>
+        <MenuIcon />
+      </IconButton>
+      <Typography variant="h6" className={"typo"}>Gaia's Garden</Typography>
+      <Badge color="secondary" badgeContent={itemCount}>
+      <ShoppingCartIcon paddingLeft={"100vh"}/>
+      </Badge>
+    </Toolbar>
+  </AppBar>
+    )
   };
   
   return (
