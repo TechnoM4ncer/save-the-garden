fb365f23f45cab0c1d31d090a7dee37a50e5205f
TechnoM4ncer <kinodekirakashi@live.com>
2022-01-27 12:20:41 -0600

    Empty header contents

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,29 +1,7 @@
-import { AppBar, Toolbar, Typography, IconButton, Badge } from "@material-ui/core";
-import { Menu as MenuIcon } from "@material-ui/icons";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import './Header.css';
 
-const itemCount = 0;
 export default function Header() {
-    const displayDesktop = () => {
-    return ( 
-    <AppBar position="static" className={"header"}>
-    <Toolbar>
-      <IconButton edge="start" color="inherit" aria-label="menu" className={"menu"}>
-        <MenuIcon />
-      </IconButton>
-      <Typography variant="h6" className={"typo"}>Gaia's Garden</Typography>
-      <Badge color="secondary" badgeContent={itemCount}>
-      <ShoppingCartIcon paddingLeft={"100vh"}/>
-      </Badge>
-    </Toolbar>
-  </AppBar>
-    )
-  };
-  
   return (
-    <header>
-      <AppBar className={"header"} >{displayDesktop()}</AppBar>
-    </header>
-  );
+    <p>Gaia's Garden</p>
+  )
 }
