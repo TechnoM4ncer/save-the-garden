ef5112913d0b02953f11740fdc51096041ed5e5a
MadiVail <virtualmadi3@gmail.com>
2022-02-01 15:41:20 -0600

    updates

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,9 +1,19 @@
 import '../App.css';
+import susearth from '../Assets/susearth.png';
 
 export default function Body() {
   return (
     <body className = {"body"}>
-        <p> UNDER CONSTRUCTION</p>
+        <h1>Gaia's Garden</h1>
+        <img src = {susearth} width = "100%" />
+        <h1>What is Sustainability?</h1>
+        <p>Sustainability is the balance between the environment, equity and economy.</p>
+        <p>It's the integration of environmental health, social equity and economic vitality in order to create thriving, healthy, diverse and resilient communities for this generation and generations to come. The practice of sustainability recognizes how these issues are interconnected and requires a systems approach and an acknowledgement of complexity.</p>
+        <p>Sustainability as a concept recognizes that the environment is an exhaustible resource. Therefore, it is important to use the environment and its resources rationally and protect it for the good of the Earth, our environment, humanity, and all living things.</p>
+        <h1>Why You Should Care About Being Sustainable!</h1>
+        <p>Being sustainable makes sense!</p>
+        <p>It can save you money, improve health, and increase safety. Being committed to sustainability will reduce your carbon footprint and the amount of toxins released into the environment, making it safe. When we focus on sustainability, the entire world benefits and gets to live in clean, more healthy living conditions.</p>
+
     </body>
   );
 }
