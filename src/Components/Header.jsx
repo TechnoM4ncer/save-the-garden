6c2f805b16237527006616d2da690e9c90d1158d
TechnoM4ncer <kinodekirakashi@live.com>
2022-01-31 19:19:45 -0600

    Add shopping cart icon

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,24 +1,26 @@
 import '../App.css';
 import { Navbar, NavItem, NavDropdown, MenuItem, Nav, Form, FormControl, Button } from 'react-bootstrap';
 import { Link, IndexLink } from 'react-router-dom';
-import * as Icon from 'react-bootstrap-icons';
+import shoppingcart from '../Assets/shoppingcart.png';
+
+
 
 export default function Header() {
   return (
 <Navbar bg="dark" variant="dark">
   <Nav className="container-fluid">
     <Nav.Item>
-      <Navbar.Brand as={Link} to="/">Demo App</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">Gaia's Garden</Navbar.Brand>
     </Nav.Item>
     <Nav.Item>
-      <Nav.Link as={Link} to="/user-list">User List</Nav.Link>
+      <Nav.Link as={Link} to="/user-list">Shop</Nav.Link>
     </Nav.Item>
     <Nav.Item>
-      <Nav.Link>Log Out</Nav.Link>
+      <Nav.Link>About</Nav.Link>
     </Nav.Item>
     <Nav.Item className="ms-auto">
       <Nav pullRight> 
-        <img src="src/Assets/shoppingcart.png" alt="Shopping Cart" width="32" height="32"/>
+        <img src={shoppingcart} alt="Shopping Cart" width="32" height="32"/>
        
       </Nav>
     </Nav.Item>
