4f9d317768dc930e9766fab1af5b78cc24112fc8
MadiVail <virtualmadi3@gmail.com>
2022-02-09 13:52:18 -0600

    added images to about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,9 @@
 import '../App.css';
 import '../index.css';
 import Header from './Header.jsx';
+import leafdrop from '../Assets/leafdrop.png';
+import sustree from '../Assets/sustree.png';
+import lightbulb from '../Assets/lightbulb.png';
 
 export default function About() {
   return (
@@ -16,12 +19,22 @@ export default function About() {
           <p>It's the integration of environmental health, social equity and economic vitality in order to create thriving, healthy, diverse and resilient communities for this generation and generations to come. The practice of sustainability recognizes how these issues are interconnected and requires a systems approach and an acknowledgement of complexity.</p>
           <p>Sustainability as a concept recognizes that the environment is an exhaustible resource. Therefore, it is important to use the environment and its resources rationally and protect it for the good of the Earth, our environment, humanity, and all living things.</p>
 
+          <img src = {sustree} width = '200px' height = '200px'/>
+
           <hr class = "class-1"/>
           <h1>Why Should You Care About Being Sustainable?</h1>
           <hr class = "class-1"/>
           <p>Being sustainable makes sense!</p>
           <p>It can save you money, improve health, and increase safety. Being committed to sustainability will reduce your carbon footprint and the amount of toxins released into the environment, making it safe. When we focus on sustainability, the entire world benefits and gets to live in clean, more healthy living conditions.</p>
           <hr class = "class-1"/>
+
+          <img src = {lightbulb} width = '180px' height = '200px'/>
+
+          <h1>Our Mission</h1>
+          <hr class = "class-1"/>
+          <p>We created an easy way for you to make your everyday life more sustainable. By bringing sustainable and eco friendly brands to one site, to make your shopping experience a breeze!</p>
+          <hr class = "class-1"/>
+          <img src = {leafdrop} width = '150px' height = '150px'/>
 
         </div>
     </body>
